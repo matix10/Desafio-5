7c1add4bd0afb49751394f00bd60b5e23c1e87be
matix10 <matiasolivar@outlook.com>
2023-08-04 16:13:53 -0300

    v2.1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.11.0"
     }
   }
@@ -10,48 +10,48 @@ terraform {
 provider "aws" {
   # Configuration options
 
-    resource "aws_security_group" "security-gp" {
-        name    = "security-gp-windows"
-        Description =   "Development security group for windows servers"
-        vpc_id  =   "vpc-0b06f8a3ec918052f"
-        ingress{
-            cidr_ipv4   = ["172.20.0.0/0"]
-            from_port   =   3389
-            ip_protocol =   "tcp"
-            to_port =   3389
-        }
+  resource "aws_security_group" "security-gp" {
+    name        = "security-gp-windows"
+    Description = "Development security group for windows servers"
+    vpc_id      = "vpc-0b06f8a3ec918052f"
+    ingress {
+      cidr_ipv4   = ["172.20.0.0/0"]
+      from_port   = 3389
+      ip_protocol = "tcp"
+      to_port     = 3389
+    }
 
-        egress{
-            
-            from_port   =   0
-            ip_protocol =   "-1"
-            to_port =   0
-            cidr_ipv4   =   ["0.0.0.0/24"]
-        }
+    egress {
+
+      from_port   = 0
+      ip_protocol = "-1"
+      to_port     = 0
+      cidr_ipv4   = ["0.0.0.0/24"]
+    }
 
 
+
+  }
+
+
+  resource "aws_iwnstance" "windows_server" {
+    name                        = "Win-Srv"
+    description                 = "Servidor credo para desafio 5"
+    ami                         = "ami-0fc682b2a42e57ca2"
+    instance_type               = "t2.micro"
+    vpc_security_group_ids      = [aws_security_group.security-gp.id]
+    key_name                    = "WinSrv-Devops"
+    associate_public_ip_address = true
+    subnet_id                   = "private_subnet_1"
+    volume_size                 = 5
+    tags = {
+      Name  = "Win-srv"
+      Owner = "matiasolivar@outlook.com"
 
     }
 
 
-    resource "aws_iwnstance" "windows_server" {
-        name    = "Win-Srv"
-        description =   "Servidor credo para desafio 5"
-        ami =   "ami-0fc682b2a42e57ca2"
-        instance_type   =   "t2.micro"
-        vpc_security_group_ids  =   [aws_security_group.security-gp.id]
-        key_name    =   "WinSrv-Devops"
-        associate_public_ip_address =   true
-        subnet_id   =   "private_subnet_1"
-
-        tags    ={
-            Name=   "Win-srv"
-            Owner   = "matiasolivar@outlook.com"
-
-        }
-
-
-    }
+  }
 
 
 }
